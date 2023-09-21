55b67511df2a2ccd34032c0f4db0de3453946296
nabila-nuzhat <e.nabilanuzhat@gmail.com>
2023-09-21 19:56:44 +0600

    Provider

--- src/components/Grandpa/Grandpa.jsx
+++ src/components/Grandpa/Grandpa.jsx
@@ -3,19 +3,43 @@ import Father from '../Father/Father';
 import Uncle from '../Uncle/Uncle';
 import Aunty from '../Aunty/Aunty';
 import './Grandpa.css'
+import { createContext } from 'react';
+
+export const RingContext = createContext('gold');
+
 const Grandpa = () => {
 
     const ring = 'diamond';
+// Module: 56-6: creating Context
+
+
     return (
         <div className='grandpa'>
             <h2>Grandpa</h2>
-            <section className='flex'>
+            <section className='flex grandpa-1'>
                 <Father ring={ring} ></Father>
                 <Uncle></Uncle>
                 <Aunty ring={ring}></Aunty>
             </section>
+
+    {/* Step 3: Provide the context */}
+            <RingContext.Provider value='golden Ring'>
+                <section className='flex grandpa-2'>
+                    <Father ring={ring} ></Father>
+                    <Uncle></Uncle>
+                    <Aunty ring={ring}></Aunty>
+                </section>
+            </RingContext.Provider>
         </div>
     );
 };
 
 export default Grandpa;
+
+
+
+/**
+ * 1. Step 1: Create the context 
+ * 2. Create a provider and pass a value 
+ * 3. use useContext to receive 
+ */
--- src/components/Special/Special.jsx
+++ src/components/Special/Special.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 const Special = ({ring}) => {
+    const angti = useContext()
     return (
         <div>
             <h2>Special </h2>
-            <p><small>Ring: {ring} </small></p>
+            <p><small>Ring: {ring}</small></p>
         </div>
     );
 };
